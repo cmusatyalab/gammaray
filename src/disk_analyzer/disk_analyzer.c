4bc7f0b306791a223b6de43b17ee92bbc445e307
Wolfgang Richter <wolf@cs.cmu.edu>
2012-02-10 15:23:24 -0500

    colorizing output of disk_analyzer

--- src/disk_analyzer/disk_analyzer.c
+++ src/disk_analyzer/disk_analyzer.c
@@ -6,7 +6,6 @@
  *****************************************************************************/
 
 
-#define UINT_8(b) ((uint8_t) b)
 #define UINT_16(s) *((uint16_t *) &s)
 #define UINT_32(i) *((uint32_t *) &i)
 
@@ -15,9 +14,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_partition_type(char type)
+#include "color.h"
+
+void print_partition_type(uint8_t type)
 {
-    char* LUT[] = { "","","","","","","","","","","","W95 FAT32","","","","", /* 0x00 - 0x0f */
+    char* LUT[] = { "Empty","","","","","","","HPFS/NTFS","","","","W95 FAT32","","","","", /* 0x00 - 0x0f */
                     "","","","","","","","","","","","","","","","", /* 0x10 - 0x1f */
                     "","","","","","","","","","","","","","","","", /* 0x20 - 0x2f */
                     "","","","","","","","","","","","","","","","", /* 0x30 - 0x3f */
@@ -34,71 +35,92 @@ void print_partition_type(char type)
                     "","","","","","","","","","","","","","","GPT","EFI", /* 0xe0 - 0xef */
                     "","","","","","","","","","","","","","","",""  /* 0xf0 - 0xff */
                   };
-    fprintf(stdout, "Partition Type: %s\n", LUT[(uint8_t) type]);
+
+    fprintf_light_white(stdout, "Partition Type: %s\n", LUT[type]);
+
     return;
+}
+
+uint8_t get_sector(uint8_t byte)
+{
+    return 0x3f & byte;
+}
+
+uint16_t get_cylinder(uint8_t* bytes)
+{
+    uint8_t b1 = bytes[0];
+    uint8_t b2 = bytes[1];
+    /* grab bits 9-0 */
+    uint16_t cylinder = (b1 & 0xc0) << 2;
+    return cylinder | b2;
 }
 
 /* prints partition entry according to Wikipedia:
  * http://en.wikipedia.org/wiki/Master_boot_record */
-void print_partition(char* start)
+void print_partition(uint8_t* start)
 {
-    fprintf(stdout, "Status [0x80 bootable, 0x00 non-bootable]: 0x%.2"
+    fprintf_blue(stdout, "Status [0x80 bootable, 0x00 non-bootable]: 0x%.2"
+                         PRIx8"\n",
+                         start[0]);
+    fprintf_blue(stdout, "Partition Type: 0x%.2"
                     PRIx8"\n",
-                    UINT_8(start[0]));
-    if (UINT_8(start[0]) == 0x00) return;
-    fprintf(stdout, "Start Head: 0x%.2"
+                    start[4]);
+    
+    print_partition_type(start[4]);
+
+    /* check it partition entry is being used */
+    if (start[4] == 0x00) return;
+    
+    fprintf_blue(stdout, "Start Head: 0x%.2"
                     PRIx8"\n",
-                    UINT_8(start[1]));
-    fprintf(stdout, "Start Sector: 0x%.2"
+                    start[1]);
+    fprintf_blue(stdout, "Start Sector: 0x%.2"
                     PRIx8"\n",
-                    UINT_8(start[2]));
-    fprintf(stdout, "Start Cylinder: 0x%.2"
+                    get_sector(start[2]));
+    fprintf_blue(stdout, "Start Cylinder: 0x%.3"
+                    PRIx16"\n",
+                    get_cylinder(&(start[2])));
+    fprintf_blue(stdout, "End Head: 0x%.2"
                     PRIx8"\n",
-                    UINT_8(start[3]));
-    fprintf(stdout, "Partition Type: 0x%.2"
+                    start[5]);
+    fprintf_blue(stdout, "End Sector: 0x%.2"
                     PRIx8"\n",
-                    UINT_8(start[4]));
-    fprintf(stdout, "End Head: 0x%.2"
-                    PRIx8"\n",
-                    UINT_8(start[5]));
-    fprintf(stdout, "End Sector: 0x%.2"
-                    PRIx8"\n",
-                    UINT_8(start[6]));
-    fprintf(stdout, "End Cylinder: 0x%.2"
-                    PRIx8"\n",
-                    UINT_8(start[7]));
-    fprintf(stdout, "First Sector LBA: 0x%.8"
+                    get_sector(start[6]));
+    fprintf_blue(stdout, "End Cylinder: 0x%.3"
+                    PRIx16"\n",
+                    get_cylinder(&(start[6])));
+    fprintf_green(stdout, "First Sector LBA: 0x%.8"
                     PRIx32"\n",
                     UINT_32(start[8]));
-    fprintf(stdout, "Number of Sectors: 0x%.8"
+    fprintf_green(stdout, "Number of Sectors: 0x%.8"
                     PRIx32"\n",
                     UINT_32(start[12]));
 
-    print_partition_type(start[4]);
     return;
 }
 
 /* main thread of execution */
 int main(int argc, char* args[])
 {
-    char mbr[512];
-    fprintf(stdout, "Raw Disk Analyzer -- By: Wolfgang Richter "
-                    "<wolf@cs.cmu.edu>\n");
+    uint8_t mbr[512];
+    fprintf_blue(stdout, "Raw Disk Analyzer -- By: Wolfgang Richter "
+                         "<wolf@cs.cmu.edu>\n");
 
     if (argc < 2)
     {
-        fprintf(stderr, "Usage: ./%s <raw disk file>\n", args[0]);
+        fprintf_light_red(stderr, "Usage: ./%s <raw disk file>\n", args[0]);
         return EXIT_FAILURE;
     }
 
     /* read Master Boot Record (MBR) */
-    fprintf(stdout, "Analyzing Disk: %s\n", args[1]);
+    fprintf_cyan(stdout, "Analyzing Disk: %s\n\n", args[1]);
 
     FILE* mbrfd = fopen(args[1], "r");
     
     if (mbrfd == NULL)
     {
-        fprintf(stderr, "Error opening raw disk file.  Does it exist?\n");
+        fprintf_light_red(stderr, "Error opening raw disk file."
+                                  "Does it exist?\n");
         return EXIT_FAILURE;
     }
 
@@ -106,27 +128,43 @@ int main(int argc, char* args[])
 
     if (read < 512)
     {
-        fprintf(stderr, "Error reading MBR from raw disk file.\n");
+        fprintf_light_red(stderr, "Error reading MBR from raw disk file.\n");
         return EXIT_FAILURE;
     }
 
     /* Taking apart according to Wikipedia:            
      * http://en.wikipedia.org/wiki/Master_boot_record */
-    fprintf(stdout, "Disk Signature [optional]: 0x%.8"PRIx32"\n",
-                    UINT_32(mbr[440]));
+    fprintf_yellow(stdout, "Disk Signature [optional]: 0x%.8"PRIx32"\n",
+                            UINT_32(mbr[440]));
 
-    fprintf(stdout, "Position 444 [0x0000]: 0x%.4"PRIx16"\n",
-                    UINT_16(mbr[444]));
+    fprintf_yellow(stdout, "Position 444 [0x0000]: 0x%.4"PRIx16"\n",
+                           UINT_16(mbr[444]));
     
-    fprintf(stdout, "Verifying MBR Signature [0x55 0xaa]: 0x%.2"PRIx8" 0x%.2"
-                    PRIx8"\n",
-                    UINT_8(mbr[510]),
-                    UINT_8(mbr[511]));
+    if (mbr[510] == 0x55 && mbr[511] == 0xaa)
+    {
+        fprintf_light_green(stdout, "Verifying MBR Signature [0x55 0xaa]: "
+                                    "0x%.2"PRIx8" 0x%.2"
+                                    PRIx8"\n\n",
+                                    mbr[510],
+                                    mbr[511]);
+    }
+    else
+    {
+        fprintf_light_red(stdout, "Verifying MBR Signature [0x55 0xaa]: 0x%.2"
+                                  PRIx8" 0x%.2"PRIx8"\n\n",
+                                  mbr[510],
+                                  mbr[511]);
+        return EXIT_FAILURE;
+    }
 
     /* read all 4 partition table entries */
+    fprintf_light_yellow(stdout, "\nChecking partition table entry 1.\n");
     print_partition(&(mbr[446]));
+    fprintf_light_yellow(stdout, "\nChecking partition table entry 2.\n");
     print_partition(&(mbr[462]));
+    fprintf_light_yellow(stdout, "\nChecking partition table entry 3.\n");
     print_partition(&(mbr[480]));
+    fprintf_light_yellow(stdout, "\nChecking partition table entry 4.\n");
     print_partition(&(mbr[496]));
 
     return EXIT_SUCCESS;
