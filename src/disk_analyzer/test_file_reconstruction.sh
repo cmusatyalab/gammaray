60b2c778f207ef5e9726aa518947c0d9f2e3ff1b
Wolfgang Richter <wolf@cs.cmu.edu>
2012-02-21 22:44:56 -0500

    disk_analyzer: test reconstruction script now also shows reference sha256sums

--- src/disk_analyzer/test_file_reconstruction.sh
+++ src/disk_analyzer/test_file_reconstruction.sh
@@ -2,7 +2,13 @@
 
 list=(`make test | grep Reconstructed | cut -d ':' -f 2 | tr -d ' '`)
 
+echo '--- Computed sha256sums from Reconstructed Files ---\n'
+
 for l in $list
 do
     sha256sum $l
 done
+
+echo '\n--- Reference sha256sums from Original Disk Image---\n'
+
+cat ../../sample/gold_sha256.list
