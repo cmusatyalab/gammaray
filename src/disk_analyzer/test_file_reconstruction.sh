54d97c7bf2c87db2e15127ac292d4054b9773624
Wolfgang Richter <wolf@cs.cmu.edu>
2012-02-28 22:44:28 -0800

    disk_analyzer: updating test script to match new debug output

--- src/disk_analyzer/test_file_reconstruction.sh
+++ src/disk_analyzer/test_file_reconstruction.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-list=(`make test | grep Reconstructed | cut -d ':' -f 2 | tr -d ' '`)
+list=(`make test | grep 'Creating file' | cut -d ':' -f 2 | tr -d ' '`)
 
 echo '--- Computed sha256sums from Reconstructed Files ---\n'
 
