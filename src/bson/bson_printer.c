a7de5f671887d407042914526a3ead3523661988
Wolfgang Richter <wolf@cs.cmu.edu>
2012-04-12 11:33:01 -0400

    bson: printer tool doesn't actually need to open a file at all anymore

--- src/bson/bson_printer.c
+++ src/bson/bson_printer.c
@@ -6,7 +6,6 @@
 
 int main(int argc, char* argv[])
 {
-    FILE* bsonf;
     struct bson_info* bson;
 
     fprintf_blue(stdout, "BSON Printer -- By: Wolfgang Richter "
@@ -20,20 +19,10 @@ int main(int argc, char* argv[])
 
     fprintf_cyan(stdout, "Analyzing BSON File: %s\n", argv[1]);
 
-    bsonf = fopen(argv[1], "r");
-
-    if (bsonf == NULL)
-    {
-        fprintf_light_red(stderr, "Error opening BSON file '%s'.\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
     bson = bson_init();
     bson_read(bson, argv[1]);
     bson_print(stdout, bson);
     bson_cleanup(bson);
-
-    fclose(bsonf);
 
     return EXIT_SUCCESS;
 }
