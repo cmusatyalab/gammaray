7df1681ad51762f413932d055cce7e9b0dbf6283
Wolfgang Richter <wolf@cs.cmu.edu>
2012-04-18 23:09:17 -0400

    tools: making tail command more generic (not tail yet though)

--- src/tools/tail.py
+++ src/tools/tail.py
@@ -13,16 +13,21 @@ if __name__ == '__main__':
     server = sys.argv[1]
     filter_string = sys.argv[2]
 
+    print 'Connecting to server \'%s\' with filter \'%s\'' % (server,
+                                                              filter_string)
+
     context= zmq.Context(1)
     socket = context.socket(zmq.SUB)
 
     socket.connect('tcp://%s:13738' % server)
     socket.setsockopt(zmq.SUBSCRIBE, filter_string)
     while (1):
-        msg = socket.recv()[len(filter_string):]
+        msg = socket.recv()
+        msg = msg[msg.find('\x00') + 1:]
         deserialized = bson.loads(msg)
-        print deserialized['type']
-        print deserialized['start_byte']
-        print deserialized['end_byte']
+        for k in deserialized:
+            if k != 'data':
+                print '%s : ' % (k),
+                print deserialized[k]
 
     context.term()
