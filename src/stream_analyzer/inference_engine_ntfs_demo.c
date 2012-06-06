d49c0d44b2ee88b8018171c40baa2877d8297e4d
Wolfgang Richter <wolf@cs.cmu.edu>
2012-06-06 15:58:46 -0400

    stream_analyzer: finalizing demo

--- src/stream_analyzer/inference_engine_ntfs_demo.c
+++ src/stream_analyzer/inference_engine_ntfs_demo.c
@@ -7,6 +7,8 @@
 
 #include "color.h"
 #include "deep_inspection.h"
+#include "ntfs.h"
+#include "util.h"
 
 #include <zmq.h>
 
@@ -22,8 +24,47 @@
 
 #define PUB_SOCKET 13738
 #define SECTOR_SIZE 512 
+#define OFFSET_OF_INTEREST 3370612736 
 
-int read_loop(int fd, struct mbr* mbr, void* pub_socket, char* vmname)
+void qemu_print_sector_type_ntfs(int type)
+{
+    switch(type)
+    {
+        default:
+            return;
+    };
+}
+
+int qemu_infer_sector_type_ntfs(struct qemu_bdrv_write* write)
+{
+    return 0;
+}
+
+int qemu_deep_inspect_ntfs(struct qemu_bdrv_write* write, void* pub_socket,
+                           char* vmname, struct ntfs_boot_file* bootf,
+                           int64_t partition_offset, uint8_t* original)
+{
+    uint64_t offset;
+
+    if (write->header.sector_num * SECTOR_SIZE <= OFFSET_OF_INTEREST &&
+        write->header.sector_num * SECTOR_SIZE + (write->header.nb_sectors) *
+        SECTOR_SIZE >= OFFSET_OF_INTEREST)
+    {
+        offset = OFFSET_OF_INTEREST - (write->header.sector_num * SECTOR_SIZE);
+        fprintf_light_yellow(stdout, "offset of interest in the write: %"PRIu64"\n", OFFSET_OF_INTEREST);
+        fprintf_light_green(stdout, "write start sector pos: %"PRIu64"\n", write->header.sector_num * SECTOR_SIZE);
+        fprintf_light_blue(stdout, "Checking offset in the write: %"PRIu64"\n", offset);
+        fprintf_light_red(stdout, "Potential write to C:/Documents and Settings/wolf/Desktop/test.txt metadata.\n");
+        ntfs_diff_file_record_buffs(original, (uint8_t*) &(write->data[offset]), partition_offset, bootf);
+        hexdump((uint8_t*) &(write->data[offset]), ntfs_file_record_size(bootf));
+    }
+
+    return 0;
+}
+
+int read_loop(int fd, void* pub_socket, char* vmname,
+              struct ntfs_boot_file* bootf, int64_t partition_offset,
+              uint8_t* original)
 {
     uint8_t buf[QEMU_HEADER_SIZE];
     int64_t total = 0, read_ret = 0;
@@ -86,12 +127,11 @@ int read_loop(int fd, struct mbr* mbr, void* pub_socket, char* vmname)
             return EXIT_FAILURE;
         }
 
-        qemu_print_write(&write);
-        qemu_print_sector_type(qemu_infer_sector_type(&write, mbr));
-        qemu_deep_inspect(&write, mbr, pub_socket, vmname);
+        //qemu_print_write(&write);
+        qemu_print_sector_type_ntfs(qemu_infer_sector_type_ntfs(&write));
+        qemu_deep_inspect_ntfs(&write, pub_socket, vmname, bootf, partition_offset, original);
         free((void*) write.data);
         fflush(stdout);
-        sleep(1);
     }
 
     return EXIT_SUCCESS;
@@ -105,45 +145,91 @@ void print_zmq_version()
     fprintf(stdout, "Current 0MQ version is %d.%d.%d\n", major, minor, patch);
 }
 
+int load_file_record(char* originalfname, uint8_t* original, uint64_t size)
+{
+    FILE* f = fopen(originalfname, "r");
+
+    if (f)
+    {
+        if (fread(original, size, 1, f) == 1)
+        {
+            fclose(f);
+            return EXIT_SUCCESS;
+        }
+    fclose(f);
+    }
+
+    return EXIT_FAILURE;
+}
+
+int load_bootf(char* bootffname, struct ntfs_boot_file* bootf)
+{
+    FILE* f = fopen(bootffname, "r");
+
+    if (f)
+    {
+        if (fread(bootf, sizeof(*bootf), 1, f) == 1)
+        {
+            fclose(f);
+            return EXIT_SUCCESS;
+        }
+        fclose(f);
+    }
+
+    return EXIT_FAILURE;
+}
+
 /* main thread of execution */
 int main(int argc, char* args[])
 {
     int fd, ret;
-    char* index, *stream, *vmname;
-    struct mbr mbr;
-    FILE* indexf;
+    char* originalfname, *bootffname, *stream, *vmname;
+    int64_t partition_offset;
+    struct ntfs_boot_file bootf;
+    uint8_t* original;
     fprintf_blue(stdout, "VM Disk Analysis Engine -- "
                          "By: Wolfgang Richter "
                          "<wolf@cs.cmu.edu>\n");
     print_zmq_version();
 
-    if (argc < 4)
+    if (argc < 6)
     {
-        fprintf_light_red(stderr, "Usage: %s <disk index file> <stream file>"
+        fprintf_light_red(stderr, "Usage: %s <original fr> <bootf> "
+                                  "<partition offset> <stream file>"
                                   " <vmname>\n", args[0]);
         return EXIT_FAILURE;
     }
 
-    index = args[1];
-    stream = args[2];
-    vmname = args[3];
+    originalfname = args[1];
+    bootffname = args[2];
+    sscanf(args[3], "%"SCNx64, &partition_offset);
+    stream = args[4];
+    vmname = args[5];
 
-    fprintf_cyan(stdout, "%s: loading index: %s\n\n", vmname, index);
+    fprintf_cyan(stdout, "Using partition offset: 0x%0.16"PRIx64"\n", partition_offset);
 
-    indexf = fopen(index, "r");
-
-    if (indexf == NULL)
+    if (load_bootf(bootffname, &bootf))
     {
-        fprintf_light_red(stderr, "Error opening index file. "
-                                  "Does it exist?\n");
+        fprintf_light_red(stderr, "Error loading boot file [%s].\n", bootffname);
         return EXIT_FAILURE;
     }
 
-    if (qemu_load_index(indexf, &mbr))
+    fprintf_cyan(stdout, "Size of a FILE Record: %"PRIu64"\n",
+                         ntfs_file_record_size(&bootf));
+    original = (uint8_t*) malloc(ntfs_file_record_size(&bootf));
+
+    if (original == NULL)
     {
-        fprintf_light_red(stderr, "Error deserializing index.\n");
+        fprintf_light_red(stderr, "Out of Memory error.\n");
         return EXIT_FAILURE;
     }
+
+    if (load_file_record(originalfname, original, ntfs_file_record_size(&bootf)))
+    {
+        fprintf_light_red(stderr, "Error loading original FILE Record.\n");
+        return EXIT_FAILURE;
+    }
+
 
     /* ----------------- 0MQ ----------------- */
     void* zmq_context = zmq_init(1);
@@ -182,9 +268,8 @@ int main(int argc, char* args[])
         return EXIT_FAILURE;
     }
 
-    ret = read_loop(fd, &mbr, pub_socket, vmname);
+    ret = read_loop(fd, pub_socket, vmname, &bootf, partition_offset, original);
     close(fd);
-    fclose(indexf);
     zmq_close(pub_socket);
     zmq_term(zmq_context);
 
