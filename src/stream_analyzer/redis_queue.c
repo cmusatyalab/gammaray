065ab222ee9bc5603bd2622608712618c96053f2
Wolfgang Richter <wolf@cs.cmu.edu>
2012-09-06 12:08:23 -0400

    stream_analyzer: adding redis list handlers

--- src/stream_analyzer/redis_queue.c
+++ src/stream_analyzer/redis_queue.c
@@ -266,6 +266,57 @@ int redis_get_fcounter(struct kv_store* handle, uint64_t* counter)
     return check_redis_return(handle->connection, reply);
 }
 
+int redis_list_get(struct kv_store* handle, char* fmt, uint64_t src,
+                   uint8_t** result[], size_t* len)
+{
+    uint64_t i;
+    redisReply* reply;
+    redis_flush_pipeline(handle);
+    reply = redisCommand(handle->connection, fmt, src);
+
+    if (reply->type == REDIS_REPLY_ARRAY)
+    {
+        *len = reply->elements;
+        *result = malloc(sizeof(uint8_t*) * (*len));
+
+        for (i = 0; i < *len; i++)
+        {
+            if (reply->element[i]->type != REDIS_REPLY_STRING)
+            {
+                check_redis_return(handle->connection, reply);
+                free(*result);
+                *result = NULL;
+                return EXIT_FAILURE;
+            }
+
+            (*result)[i] = (uint8_t*) malloc(reply->len + 1);
+
+            memcpy((*result)[i], reply->element[i]->str,
+                   (size_t) reply->element[i]->len);
+
+            (*result)[i][reply->element[i]->len] = 0;
+        }
+    }
+
+    return check_redis_return(handle->connection, reply);
+}
+
+
+void redis_free_list(uint8_t* list[], size_t len)
+{
+    size_t i;
+
+    if (list)
+    {
+        for (i = 0; i < len; i++)
+        {
+            if (list[i])
+                free(list[i]);
+        }
+        free(list);
+    }
+}
+
 void redis_shutdown(struct kv_store* handle)
 {
     if (handle)
--- src/stream_analyzer/redis_queue.h
+++ src/stream_analyzer/redis_queue.h
@@ -15,7 +15,8 @@
 
 #define REDIS_BGD_SECTOR_INSERT "HSET bgd:%"PRIu64" %s %b"
 #define REDIS_BGD_SECTOR_GET "HGET bgd:%"PRIu64" %s"
-#define REDIS_BGDS_INSERT "LPUSH bgds:%"PRIu64" bgd:%"PRIu64
+#define REDIS_BGDS_INSERT "RPUSH bgds:%"PRIu64" bgd:%"PRIu64
+#define REDIS_BGDS_LGET "LRANGE bgds:%"PRIu64" 0 -1"
 #define REDIS_BGDS_SECTOR_INSERT "SET sector:%"PRIu64" lbgds:%"PRIu64
 
 #define REDIS_BLOCKDMAP_INSERT "SET sector:%"PRIu64" bgd:%"PRIu64
@@ -23,12 +24,14 @@
 
 #define REDIS_INODE_SECTOR_INSERT "HSET inode:%"PRIu64" %s %b"
 #define REDIS_INODE_SECTOR_GET "HGET inode:%"PRIu64" %s"
-#define REDIS_INODES_INSERT "LPUSH inodes:%"PRIu64" inode:%"PRIu64
+#define REDIS_INODES_INSERT "RPUSH inodes:%"PRIu64" inode:%"PRIu64
+#define REDIS_INODES_LGET "LRANGE inodes:%"PRIu64" 0 -1"
 #define REDIS_INODES_SECTOR_INSERT "SET sector:%"PRIu64" linodes:%"PRIu64
 
 #define REDIS_EXTENT_SECTOR_INSERT "HSET extent:%"PRIu64" %s %b"
 #define REDIS_EXTENT_SECTOR_GET "HGET extent:%"PRIu64" %s"
-#define REDIS_EXTENTS_INSERT "LPUSH extents:%"PRIu64" extent:%"PRIu64
+#define REDIS_EXTENTS_INSERT "RPUSH extents:%"PRIu64" extent:%"PRIu64
+#define REDIS_EXTENTS_LGET "LRANGE extents:%"PRIu64" 0 -1"
 #define REDIS_EXTENTS_SECTOR_INSERT "SET sector:%"PRIu64"' lextents:%"PRIu64
 
 
@@ -54,10 +57,14 @@ int redis_reverse_pointer_set(struct kv_store* handle, const char* fmt,
 int redis_hash_field_set(struct kv_store* handle, const char* fmt,
                          uint64_t src, char* field, uint8_t* data, size_t len);
 int redis_hash_field_get(struct kv_store* handle, const char* fmt,
-                         uint64_t src, char* field, uint8_t* data, size_t* len);
+                         uint64_t src, char* field, uint8_t* data,
+                         size_t* len);
 int redis_reverse_file_data_pointer_set(struct kv_store* handle,
                                         uint64_t src, uint64_t start,
                                         uint64_t end, uint64_t dst);
 int redis_sector_lookup(struct kv_store* store, uint64_t sector, uint8_t* data,
                         size_t* len);
+int redis_list_get(struct kv_store* handle, char* fmt, uint64_t src,
+                   uint8_t** result[], size_t* len);
+void redis_free_list(uint8_t* list[], size_t len);
 #endif
