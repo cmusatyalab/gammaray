026aaf954959f9eb72e889ec085307685e8edfb0
Wolfgang Richter <wolf@cs.cmu.edu>
2013-02-04 12:55:48 -0500

    fuse_driver: adding xray_fs file driver header

---
+++ src/fuse_driver/xray_fs.h
@@ -0,0 +1,22 @@
+#ifndef __XRAY_FS_H_
+#define __XRAY_FS_H_
+
+#define FUSE_USE_VERSION 28
+
+#include <fuse.h>
+
+static int xrayfs_getattr(const char* path, struct stat* stbuf);
+static int xrayfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
+                          off_t offset, struct fuse_file_info* fi);
+static int xrayfs_open(const char* path, struct fuse_file_info* fi);
+static int xrayfs_read(const char* path, char* buf, size_t size, off_t offset,
+                       struct fuse_file_info* fi);
+
+static struct fuse_operations xrayfs_oper = {
+                                                .getattr = xrayfs_getattr,
+                                                .readdir = xrayfs_readdir,
+                                                .open    = xrayfs_open,
+                                                .read    = xrayfs_read
+                                             };
+
+#endif
