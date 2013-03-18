ba3693f33ac5806e29e59c14d1505119d68b51f1
Wolfgang Richter <wolf@cs.cmu.edu>
2013-03-18 12:59:03 -0400

    disk_analyzer: exposing function for dynamic use

--- src/disk_analyzer/ext4/ext4.h
+++ src/disk_analyzer/ext4/ext4.h
@@ -227,6 +227,7 @@ int ext4_print_sectormap(FILE* disk, int64_t partition_offset,
 int64_t ext4_sector_from_block(uint64_t block, struct ext4_superblock super,
                                int64_t partition_offset);
 char* ext4_last_mount_point(struct ext4_superblock* superblock);
+uint64_t ext4_s_blocks_count(struct ext4_superblock superblock);
 int ext4_serialize_fs(struct ext4_superblock* superblock, int64_t offset,
                       int32_t pte_num, struct bitarray* bits,
                       char* mount_point, FILE* serializef);
