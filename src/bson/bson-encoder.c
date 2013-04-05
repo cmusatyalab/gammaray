9d80b9225bb054b4d28958963f0ab9db3bebb1fe
Wolfgang Richter <wolf@cs.cmu.edu>
2013-04-05 12:16:35 -0400

    bson: adding LICENSE info to all files

--- src/bson/bson-encoder.c
+++ src/bson/bson-encoder.c
@@ -1,8 +1,28 @@
-/*
- * This file implements the BSON (v1.0) standard as defined here:
- *  http://bsonspec.org/#/specification
- *
- */
+/*****************************************************************************
+ * bson-encoder.h                                                            *
+ *                                                                           *
+ * This file contains implementations for a C BSON encoder.  It supports     *
+ * packing documents together in a single file or stream.  It implements the *
+ * BSON (v1.0) standard as defined here: http://bsonspec.org/#/specification *
+ *                                                                           *
+ *                                                                           *
+ *   Authors: Wolfgang Richter <wolf@cs.cmu.edu>                             *
+ *                                                                           *
+ *                                                                           *
+ *   Copyright 2013 Carnegie Mellon University                               *
+ *                                                                           *
+ *   Licensed under the Apache License, Version 2.0 (the "License");         *
+ *   you may not use this file except in compliance with the License.        *
+ *   You may obtain a copy of the License at                                 *
+ *                                                                           *
+ *       http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                           *
+ *   Unless required by applicable law or agreed to in writing, software     *
+ *   distributed under the License is distributed on an "AS IS" BASIS,       *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *   See the License for the specific language governing permissions and     *
+ *   limitations under the License.                                          *
+ *****************************************************************************/
 #include "bson.h"
 #include "__bson.h"
 
--- src/bson/bson.h
+++ src/bson/bson.h
@@ -1,5 +1,29 @@
-#ifndef __BSON_C_BSON_H
-#define __BSON_C_BSON_H
+/*****************************************************************************
+ * bson.h                                                                    *
+ *                                                                           *
+ * This file contains prototypes for functions implementing a BSON encoder   *
+ * and decoder library in C.                                                 *
+ *                                                                           *
+ *                                                                           *
+ *   Authors: Wolfgang Richter <wolf@cs.cmu.edu>                             *
+ *                                                                           *
+ *                                                                           *
+ *   Copyright 2013 Carnegie Mellon University                               *
+ *                                                                           *
+ *   Licensed under the Apache License, Version 2.0 (the "License");         *
+ *   you may not use this file except in compliance with the License.        *
+ *   You may obtain a copy of the License at                                 *
+ *                                                                           *
+ *       http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                           *
+ *   Unless required by applicable law or agreed to in writing, software     *
+ *   distributed under the License is distributed on an "AS IS" BASIS,       *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *   See the License for the specific language governing permissions and     *
+ *   limitations under the License.                                          *
+ *****************************************************************************/
+#ifndef __GAMMARAY_BSON_H
+#define __GAMMARAY_BSON_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -164,9 +188,9 @@ bson_readf(struct bson_info* bson_info, FILE* file);
  /**
   * bson_make_readable
   *
-  * This function makes a bson_info struct ready to be deserialized.  Useful for
-  * in-memory passing that is not hitting disk (bson_read does the requisite
-  * work).
+  * This function makes a bson_info struct ready to be deserialized.  Useful
+  * for in-memory passing that is not hitting disk (bson_read does the
+  * requisite work).
   * 
   *
   * @param bson_info - the metadata structure to read into
