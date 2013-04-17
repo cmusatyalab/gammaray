e904122ee94c67b551b8fa6a6c2772821ff4ae52
Wolfgang Richter <wolf@cs.cmu.edu>
2013-04-17 11:31:56 -0400

    gammaray: refactor libqemucommon; add in build for fuse_driver

--- src/inference_engine/build.mk
+++ src/inference_engine/build.mk
@@ -6,7 +6,8 @@ lib_libredis_la_SOURCES = src/inference_engine/redis_queue.c
 lib_libredis_la_LIBADD = $(libdir)/libbitarray.la $(libdir)/libutil.la
 lib_libredis_la_CFLAGS = $(AM_CFLAGS) -I/usr/include/hiredis
 
-lib_libqemucommon_la_SOURCES = src/inference_engine/qemu_common.c
+lib_libqemucommon_la_SOURCES = src/inference_engine/qemu_common.c \
+							   src/inference_engine/deep_inspection.c
 lib_libqemucommon_la_LIBADD = $(libdir)/libbson.la
 
 bin_async_queuer_SOURCES = src/inference_engine/async_queuer.c
@@ -14,8 +15,7 @@ bin_async_queuer_LDADD = $(libdir)/libbitarray.la $(libdir)/libcolor.la \
 						 $(libdir)/libredis.la $(libdir)/libutil.la \
 						 $(libdir)/libqemucommon.la -lpthread
 
-bin_inference_engine_SOURCES = src/inference_engine/inference_engine.c \
-							   src/inference_engine/deep_inspection.c
+bin_inference_engine_SOURCES = src/inference_engine/inference_engine.c
 bin_inference_engine_LDADD = $(libdir)/libbitarray.la $(libdir)/libcolor.la \
 							 $(libdir)/libredis.la $(libdir)/libutil.la \
 							 $(libdir)/libqemucommon.la $(libdir)/libext4.la \
