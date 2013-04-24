ba427c95f604b197b1e09c0f0a1122cfa74ee9b2
Wolfgang Richter <wolf@cs.cmu.edu>
2013-04-24 17:44:25 -0400

    gray-fs: typo fix in build fragment

--- src/gray-fs/build.mk
+++ src/gray-fs/build.mk
@@ -1,6 +1,6 @@
 bin_PROGRAMS += bin/gray-fs
 
-bin_gray_fs_SOURCES = src/gray-/fs/gray-fs.c
+bin_gray_fs_SOURCES = src/gray-fs/gray-fs.c
 bin_gray_fs_LDADD   = $(libdir)/libbitarray.la \
 					  $(libdir)/libcolor.la \
 					  $(libdir)/libqemucommon.la \
