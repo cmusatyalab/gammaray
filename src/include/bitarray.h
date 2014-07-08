c359303e45b14e7e4a369e620b8b7ad71aea055e
Wolfgang Richter <wolf@cs.cmu.edu>
2014-07-08 15:51:03 -0400

    datastructures: adding bitarray support for self-serializing

--- src/include/bitarray.h
+++ src/include/bitarray.h
@@ -8,7 +8,7 @@
  *   Authors: Wolfgang Richter <wolf@cs.cmu.edu>                             *
  *                                                                           *
  *                                                                           *
- *   Copyright 2013 Carnegie Mellon University                               *
+ *   Copyright 2013-2014 Carnegie Mellon University                          *
  *                                                                           *
  *   Licensed under the Apache License, Version 2.0 (the "License");         *
  *   you may not use this file except in compliance with the License.        *
@@ -26,6 +26,7 @@
 #define __GAMMARAY_BITARRAY_H
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 struct bitarray;
@@ -40,5 +41,6 @@ struct bitarray* bitarray_init(uint64_t len);
 struct bitarray* bitarray_init_data(uint8_t* data, uint64_t len);
 void bitarray_destroy(struct bitarray* bits);
 uint64_t bitarray_get_array(struct bitarray* bits, uint8_t** array);
+int bitarray_serialize(struct bitarray* bits, FILE* serializef);
 
 #endif
